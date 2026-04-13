0f60d3f6c6788abe6cf6ae0f76c4b88b90f41658
David <jdbsjdbs340@gmail.com>
2026-04-13 18:57:58 -0500

    Ejercicio Clase

--- src/com/edu/uce/negocio/PedidoService.java
+++ src/com/edu/uce/negocio/PedidoService.java
@@ -2,14 +2,20 @@ package com.edu.uce.negocio;
 
 public class PedidoService {
 
+    private NotificadorMail n1 = new NotificadorMail();
+    private NotificadorSMS n2 = new NotificadorSMS();
+
     public void registrar(Pedido pedido) {
         System.out.println("Registrando pedido");
         System.out.println("Cliente: " + pedido.getCliente());
         System.out.println("Total del pedido: " + pedido.getTotal());
         System.out.println("Guardando en la base de datos");
 
-        NotificadorMail n1 = new NotificadorMail();
-        n1.enviar(pedido.getCorreo(), "Se ha creado un pedido para ser atentido");
-        
+        if (pedido.getTotal() > 100) {
+            n1.enviar(pedido.getCorreo(), "Se ha creado un pedido para ser atentido");
+        } else {
+            n2.enviar(pedido.getTelefono(), "Se ha creado un pedido para ser atentido");
+        }
+
     }
 }
